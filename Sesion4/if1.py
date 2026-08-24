4c5d18e8806e8360fa2a5d40aa2906248f3d9bdf
ceratiwong <michaelwong@uamv.edu.ni>
2026-08-24 07:55:24 -0600

    Se agregó la carpeta Sesion4

---
+++ Sesion4/if1.py
@@ -0,0 +1,6 @@
+#Leer la nota de un estudiante y decir aprobado o reprobado
+grade = int(input("Ingrese la nota: "))
+if grade >= 70:
+    print("Usted está aprobado.")
+else:
+    print("Usted está reprobado.")
