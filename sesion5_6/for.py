504359f774bb7180321e3e8668cd00522796d588
ceratiwong <michaelwong@uamv.edu.ni>
2026-08-28 07:45:17 -0600

    Ejercicios de for

---
+++ sesion5_6/for.py
@@ -0,0 +1,12 @@
+#Contar los números del 0 al 9
+from colorama import Fore, Style
+for numero in range(10):
+    if numero % 2 == 0:
+        print(Fore.GREEN + f"Número: {numero}" + Style.RESET_ALL)
+        for num in range(numero):
+            print(f"Antecesor: {num}")
+            for n in range(num):
+                if n % 2 != 0:
+                    print (f"{n}")
+    else:
+        print(Fore.RED + f"Número: {numero}" + Style.RESET_ALL)
